79f6d1f24184a9f6ee3d22bfb73c7f46998ca9ec
rshivam973 <rshivam973@gmail.com>
2022-10-10 17:41:50 +0530

    changed margin

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,38 +1,19 @@
-// import React from "react";
-
-// function Card(props){
-
-//     return <div>
-//     <img alt={props.img} />
-//     <h2>{props.name}</h2>
-//     </div>
-
-// }
-
-// export default Card;
-
+import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import "./Cards.css"
 
-function Cards() {
+function Cards(props) {
   return (
-    <Card className='card-render' style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+    <Card className="main-card" style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
-          Content
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
         </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
   );
