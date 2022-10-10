bf3091c3980cfe907d358e7c862515e4716d618f
rshivam973 <rshivam973@gmail.com>
2022-10-10 23:03:28 +0530

    cards working!!!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Header from "./Header/Header";
-import MoviesList from "./MoviesList/MoviesList"
+import movies from "./MoviesList/MoviesList"
 import Cards from "./Cards/Cards";
+
+function renderCard(val){
+    return (
+    <Cards
+    name={val.name}
+    img={val.img}
+     />
+    );
+
+}
+
 
 function App(){
 
     return (<div>
         <Header />
         <Navbar />
-        <Cards  
-        />
+        {movies.map(renderCard)}
+
     </div>);
 }
 
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,21 +1,20 @@
-import { Button } from 'react-bootstrap';
+import { Button, CardGroup } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
 import "./Cards.css"
 
 function Cards(props) {
   return (
-    <Card className="main-card" style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+      <Card className="main-card" style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={props.img} />
+        <Card.Body>
+          <Card.Title>{props.name}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
   );
 }
 
